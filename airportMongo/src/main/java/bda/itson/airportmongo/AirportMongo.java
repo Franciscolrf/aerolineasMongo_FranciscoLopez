eb2b4bc2ddacd5e194e3725cb6eacb396bf20791
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-04-25 16:27:49 -0700

    Proyecto creado.

---
+++ airportMongo/src/main/java/bda/itson/airportmongo/AirportMongo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bda.itson.airportmongo;
+
+/**
+ *
+ * @author ID145
+ */
+public class AirportMongo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
