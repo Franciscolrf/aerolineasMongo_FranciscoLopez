beb255551d49c54c4d10b24b6b5d6742f9342481
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-04-25 16:56:56 -0700

    Pruebas
    pruebas para probar el funcionamiento del proyecto.

--- airportMongo/src/main/java/bda/itson/airportmongo/AirportMongo.java
+++ airportMongo/src/main/java/bda/itson/airportmongo/AirportMongo.java
@@ -1,8 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package bda.itson.airportmongo;
+
+import datos.DAOAerolinea;
+import java.util.ArrayList;
+import modelos.Aerolinea;
 
 /**
  *
@@ -11,6 +14,38 @@ package bda.itson.airportmongo;
 public class AirportMongo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        DAOAerolinea a = new DAOAerolinea();
+
+        // Método agregar.
+        Aerolinea aerolinea = new Aerolinea("Volaris", "mexico", "MXN", true);
+        Aerolinea aerolinea1 = new Aerolinea("Interjet", "Mexico", "MXN", true);
+        Aerolinea aerolinea2 = new Aerolinea("American Airlines", "Estados Unidos", "USD", false);
+        Aerolinea aerolinea3 = new Aerolinea("Emirates", "Emiratos Árabes Unidos", "AED", false);
+        Aerolinea aerolinea4 = new Aerolinea("British Airways", "Reino Unido", "GBP", true);
+        a.AgregarAerolinea(aerolinea);
+        a.AgregarAerolinea(aerolinea1);
+        a.AgregarAerolinea(aerolinea2);
+        a.AgregarAerolinea(aerolinea3);
+        a.AgregarAerolinea(aerolinea4);
+
+        // Método obtener todas.
+        ArrayList<Aerolinea> lista = a.obtenerAerolineas();
+        for (Aerolinea as : lista) {
+            System.out.println(a.toString());
+        }
+
+        // Método eliminar.
+        String primerId = a.obtenerPrimerIdAerolinea();
+        if (primerId != null) {
+            System.out.println("ID del primer documento encontrado: " + primerId);
+
+            boolean eliminado = a.eliminarAerolinea(primerId);
+            if (eliminado) {
+                System.out.println("Aerolínea eliminada correctamente.");
+            } else {
+                System.out.println("La aerolínea no pudo ser eliminada.");
+            }
+        }
+
     }
 }
