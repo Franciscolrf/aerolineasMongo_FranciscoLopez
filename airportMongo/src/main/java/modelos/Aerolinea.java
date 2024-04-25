a7003d44c59d6ba2c885a9fd78be131aec367b51
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-04-25 16:37:21 -0700

    Update Aerolinea.java
    setters, getters y to string.

--- airportMongo/src/main/java/modelos/Aerolinea.java
+++ airportMongo/src/main/java/modelos/Aerolinea.java
@@ -35,5 +35,52 @@ public class Aerolinea {
         this.moneda = moneda;
         this.economica = economica;
     }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
+    }
+
+    public boolean isEconomica() {
+        return economica;
+    }
+
+    public void setEconomica(boolean economica) {
+        this.economica = economica;
+    }
+
+    @Override
+    public String toString() {
+        return "Aerolinea{" + "id=" + id + ", nombre=" + nombre + ", pais=" + pais + ", moneda=" + moneda + ", economica=" + economica + '}';
+    }
+    
+    
     
 }
